83bbf644bfc154bb6e875026cacf40ff9d65c292
Nour Essoussi <nourcherifsoussi@gmail.com>
2019-05-17 11:41:24 -0400

    Second Commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.json({ home: "Screen" });
-});
+// router.get("/", (req, res, next) => {
+//   res.json({ home: "Screen" });
+// });
 
 module.exports = router;
